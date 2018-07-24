e765f67f180bb39cc9bd8eec99b6cbc1b766f188
Ashley <ashley.ratigan@gmail.com>
2018-07-24 14:46:33 -0400

    Set up so when clicked places button now generates random city

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -1,19 +1,34 @@
 'use strict'
 
-// const config = require('../config')
+const config = require('../config')
 
-let id = 0
-$('#getPlacesButton').click(function () {
-  id = Math.floor(Math.random() * 60)
-  return id
+let planetId = 0
+$('#getPlanetsButton').click(function () {
+  planetId = Math.floor(Math.random() * 60)
+  return planetId
 })
 
-const getPlaces = function () {
+let placeId = 14645
+$('#getPlacesButton').click(function () {
+  placeId = 14645 + Math.floor(Math.random() * 7321)
+  return placeId
+})
+
+const getPlanets = function () {
+  console.log(`planetId is ${planetId}`)
   return $.ajax({
-    url: `https://swapi.co/api/planets/${id}/`
+    url: `https://swapi.co/api/planets/${planetId}/`
+  })
+}
+
+const getPlaces = function () {
+  console.log(`placeId is ${placeId}`)
+  return $.ajax({
+    url: config.apiUrl + `/places/${placeId}`
   })
 }
 
 module.exports = {
+  getPlanets,
   getPlaces
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,7 +6,7 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-const placeEvents = require('./places/events.js')
+const placeEvents = require('./auth/events.js')
 
 // On document ready
 $(() => {
