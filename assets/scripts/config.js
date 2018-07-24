d0bcdd3bd301e5c38bd5c6178161376e61ed5f45
Ashley <ashley.ratigan@gmail.com>
2018-07-24 14:58:12 -0400

    Added heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://desolate-plateau-52113.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
