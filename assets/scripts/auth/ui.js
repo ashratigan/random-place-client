962a5420a4322fadfaaa6171fd06c7266867478c
Ashley <ashley.ratigan@gmail.com>
2018-07-25 09:45:08 -0400

    Removed console.log and commented out code

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,31 +3,21 @@
 // const showPlacesTemplate = require('../templates/book-listing.handlebars')
 
 const getPlanetsSuccess = (data) => {
-  console.log(`planets is ${data.name}`)
-  console.log(`planets is ${data.terrain}`)
   $('#contentH').html(`${data.name}`)
   $('#contentP').html(`The climate here is ${data.climate} and the terrain is ${data.terrain}`)
   $('#contentP2').html(`The population is ${data.population}`)
-  // const showPlacesHtml = showPlacesTemplate({ planets: data })
-  // console.log(showPlacesHtml)
-  // $('.content').html(showPlacesHtml)
 }
 
 const getPlacesSuccess = (data) => {
-  console.log(`planets is ${data}`)
-  console.log(`planets is ${data.place.id}`)
-  console.log(`planets is ${data.place.city}`)
-  console.log(`planets is ${data.place.country}`)
   $('#contentH').html(`${data.place.city}`)
   $('#contentP').html(`Located in ${data.place.province}, ${data.place.country}`)
   $('#contentP2').html(`The population is ${data.place.pop}`)
-  // const showPlacesHtml = showPlacesTemplate({ planets: data })
-  // console.log(showPlacesHtml)
-  // $('.content').html(showPlacesHtml)
 }
 
-const failure = (error) => {
-  console.error(error)
+const failure = () => {
+  $('#contentH').html(`Oops, something went wrong!`)
+  $('#contentP').html(``)
+  $('#contentP2').html(``)
 }
 
 module.exports = {
